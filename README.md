22180c64c3432a4f75be936b106e8cd762a86fb7
Jun Aruga <jaruga@redhat.com>
2023-04-14 21:27:54 +0200

    Add ed25519 public_to_pem logic.

--- README.md
+++ README.md
@@ -1,16 +1,48 @@
 # report-openssl-fips-ed25519
 
+This is a C program to reproduce a method to read a ed25519 PEM file, then convert to a object and then convert to the PEM file again. It should be the same with the origina PEM file.
+
+```
+$ cat ed25519_pub.pem
+-----BEGIN PUBLIC KEY-----
+MCowBQYDK2VwAyEAPUAXw+hDiVqStwqnTRt+vJyYLM8uxJaMwM1V8Sr0Zgw=
+-----END PUBLIC KEY-----
+```
+
 ```
 $ gcc -o ed25519 ed25519.c -lcrypto
+```
 
+## Non-FIPS mode
+
+```
+$ OPENSSL_CONF_INCLUDE=/home/jaruga/.local/openssl-3.0.8-debug/ssl \
+  OPENSSL_MODULES=/home/jaruga/.local/openssl-3.0.8-debug/lib/ossl-modules \
+  ./ed25519 ed25519_pub.pem
+[DEBUG] Loaded providers:
+  default
+[DEBUG] Got a pkey! 0x24294e0
+[DEBUG] It's held by the provider default
+[DEBUG] ossl_membio2str buf->data:
+-----BEGIN PUBLIC KEY-----
+MCowBQYDK2VwAyEAPUAXw+hDiVqStwqnTRt+vJyYLM8uxJaMwM1V8Sr0Zgw=
+-----END PUBLIC KEY-----
+```
+
+## FIPS mode
+
+```
 $ OPENSSL_CONF=/home/jaruga/.local/openssl-3.0.8-fips-debug/ssl/openssl_fips.cnf \
   OPENSSL_CONF_INCLUDE=/home/jaruga/.local/openssl-3.0.8-fips-debug/ssl \
   OPENSSL_MODULES=/home/jaruga/.local/openssl-3.0.8-fips-debug/lib/ossl-modules \
-  /home/jaruga/git/report-openssl-fips-ed25519/ed25519 key.pem
-Loaded providers:
+  ./ed25519 ed25519_pub.pem
+[DEBUG] Loaded providers:
   fips
   base
-[DEBUG] Calling ossl_pkey_read_generic from ossl_pkey_new_from_data.
-Got a pkey! 0x1800430
-It's held by the provider fips
+[DEBUG] Got a pkey! 0x21476a0
+[DEBUG] It's held by the provider fips
+[DEBUG] ossl_membio2str buf->data:
+-----BEGIN PUBLIC KEY-----
+MCowBQYDK2VwAyEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=
+-----END PUBLIC KEY-----
 ```
--- ed25519.c
+++ ed25519.c
@@ -10,6 +10,8 @@
 /* The following is extracted from https://github.com/junaruga/openssl/raw/41bc792df2cf54660264bd6fc6368044f2877e99/ext/openssl/ossl_pkey.c and modified to get rid of Ruby specific stuff */
 # include <openssl/decoder.h>
 
+void ossl_membio2str(BIO*);
+
 EVP_PKEY *
 ossl_pkey_read_generic(BIO *bio, char *pass)
 {
@@ -29,33 +31,6 @@ ossl_pkey_read_generic(BIO *bio, char *pass)
         goto out;
     BIO_reset(bio);
 
-    /*
-     * Then check PEM; multiple OSSL_DECODER_from_bio() calls may be needed.
-     *
-     * First check for private key formats. This is to keep compatibility with
-     * ruby/openssl < 3.0 which decoded the following as a private key.
-     *
-     *     $ openssl ecparam -name prime256v1 -genkey -outform PEM
-     *     -----BEGIN EC PARAMETERS-----
-     *     BggqhkjOPQMBBw==
-     *     -----END EC PARAMETERS-----
-     *     -----BEGIN EC PRIVATE KEY-----
-     *     MHcCAQEEIAG8ugBbA5MHkqnZ9ujQF93OyUfL9tk8sxqM5Wv5tKg5oAoGCCqGSM49
-     *     AwEHoUQDQgAEVcjhJfkwqh5C7kGuhAf8XaAjVuG5ADwb5ayg/cJijCgs+GcXeedj
-     *     86avKpGH84DXUlB23C/kPt+6fXYlitUmXQ==
-     *     -----END EC PRIVATE KEY-----
-     *
-     * While the first PEM block is a proper encoding of ECParameters, thus
-     * OSSL_DECODER_from_bio() would pick it up, ruby/openssl used to return
-     * the latter instead. Existing applications expect this behavior.
-     *
-     * Note that normally, the input is supposed to contain a single decodable
-     * PEM block only, so this special handling should not create a new problem.
-     *
-     * Note that we need to create the OSSL_DECODER_CTX variable each time when
-     * we use the different selection as a workaround.
-     * https://github.com/openssl/openssl/issues/20657
-     */
     OSSL_DECODER_CTX_free(dctx);
     dctx = NULL;
     dctx = OSSL_DECODER_CTX_new_for_pkey(&pkey, "PEM", NULL, NULL,
@@ -125,15 +100,13 @@ ossl_pkey_new_from_data(char *data, char *pass)
 
     bio = BIO_new_mem_buf(data, strlen(data));
     if (!bio) {
-        ERR_raise_data(ERR_LIB_NONE, ERR_R_BIO_LIB,
-                       "BIO_new_mem_buf() failed");
+        fprintf(stderr, "[DEBUG] BIO_new_mem_buf() failed\n");
         return NULL;
     }
-    fprintf(stderr, "[DEBUG] Calling ossl_pkey_read_generic from ossl_pkey_new_from_data.\n");
     pkey = ossl_pkey_read_generic(bio, pass);
     BIO_free(bio);
     if (!pkey)
-        ERR_raise_data(ERR_LIB_NONE, 0, "Could not parse PKey");
+        fprintf(stderr, "Could not parse PKey\n");
     return pkey;
 }
 
@@ -145,12 +118,51 @@ static int print_provider(OSSL_PROVIDER *prov, void *unused)
     return 1;
 }
 
+void
+ossl_pkey_export_spki(EVP_PKEY *pkey, int to_der)
+{
+    BIO *bio;
+
+    bio = BIO_new(BIO_s_mem());
+    if (!bio)
+        fprintf(stderr, "BIO_new\n");
+    if (to_der) {
+        if (!i2d_PUBKEY_bio(bio, pkey)) {
+            BIO_free(bio);
+            fprintf(stderr, "i2d_PUBKEY_bio\n");
+        }
+    }
+    else {
+        if (!PEM_write_bio_PUBKEY(bio, pkey)) {
+            BIO_free(bio);
+            fprintf(stderr, "PEM_write_bio_PUBKEY\n");
+        }
+    }
+    ossl_membio2str(bio);
+}
+
+void
+ossl_membio2str(BIO *bio)
+{
+    BUF_MEM *buf;
+
+    BIO_get_mem_ptr(bio, &buf);
+    /*
+    ret = ossl_str_new(buf->data, buf->length, &state);
+    BIO_free(bio);
+    if (state)
+        rb_jump_tag(state);
+     */
+
+    printf("[DEBUG] ossl_membio2str buf->data:\n%s\n", buf->data);
+}
+
 int main(int argc, char *argv[])
 {
     static char data[1024 * 1024];
     EVP_PKEY *pkey;
 
-    printf("Loaded providers:\n");
+    printf("[DEBUG] Loaded providers:\n");
     OSSL_PROVIDER_do_all(NULL, &print_provider, NULL);
 
     /* Slurp */
@@ -161,16 +173,19 @@ int main(int argc, char *argv[])
         || (data_size = fread(data, 1, sizeof(data), f)) == 0) {
         if (f)
             fclose(f);
-        ERR_raise_data(ERR_LIB_NONE, 0, "Could not read PKey");
+        fprintf(stderr, "[DEBUG] Could not read PKey\n");
     } else {
         fclose(f);
         data[data_size] = '\0';
         pkey = ossl_pkey_new_from_data(data, "");
         if (pkey) {
-            printf("Got a pkey! %p\n", (void *)pkey);
-            printf("It's held by the provider %s\n",
+            printf("[DEBUG] Got a pkey! %p\n", (void *)pkey);
+            printf("[DEBUG] It's held by the provider %s\n",
                    OSSL_PROVIDER_get0_name(EVP_PKEY_get0_provider(pkey)));
         }
+
+        ossl_pkey_export_spki(pkey, 0);
+
         EVP_PKEY_free(pkey);
     }
 
