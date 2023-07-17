222f91ea3244d0c2a7077a8947cf9be843d8793d
Jun Aruga <jaruga@redhat.com>
2023-07-17 16:37:58 +0200

    Add another refactored reproducing program.
    * It's with more strict error handling.
    * Removed some functions.

---
+++ ed25519_2.c
@@ -0,0 +1,180 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <openssl/pem.h>
+#include <openssl/bio.h>
+# include <openssl/decoder.h>
+#include <openssl/provider.h>
+
+static int print_provider(OSSL_PROVIDER *prov, void *unused)
+{
+    printf("  %s\n", OSSL_PROVIDER_get0_name(prov));
+    return 1;
+}
+
+EVP_PKEY *
+ossl_pkey_read_generic(BIO *bio, char *pass)
+{
+    void *ppass = (void *)pass;
+    OSSL_DECODER_CTX *dctx;
+    EVP_PKEY *pkey = NULL;
+    int pos = 0, pos2;
+
+    dctx = OSSL_DECODER_CTX_new_for_pkey(&pkey, "DER", NULL, NULL, 0, NULL, NULL);
+    if (!dctx) {
+        fprintf(stderr, "OSSL_DECODER_CTX_new_for_pkey DER failed.\n");
+        goto out;
+    }
+    if (OSSL_DECODER_CTX_set_pem_password_cb(dctx, PEM_def_callback, ppass) != 1) {
+        fprintf(stderr, "OSSL_DECODER_CTX_set_pem_password_cb DER failed.\n");
+        goto out;
+    }
+
+    /* First check DER */
+    if (OSSL_DECODER_from_bio(dctx, bio) == 1) {
+        fprintf(stderr, "OSSL_DECODER_from_bio DER failed.\n");
+        goto out;
+    }
+    BIO_reset(bio);
+
+    OSSL_DECODER_CTX_free(dctx);
+    dctx = NULL;
+    dctx = OSSL_DECODER_CTX_new_for_pkey(&pkey, "PEM", NULL, NULL,
+                                         EVP_PKEY_KEYPAIR, NULL, NULL);
+    if (!dctx) {
+        fprintf(stderr, "OSSL_DECODER_CTX_new_for_pkey PEM failed.\n");
+        goto out;
+    }
+    if (OSSL_DECODER_CTX_set_pem_password_cb(dctx, PEM_def_callback, ppass) != 1) {
+        fprintf(stderr, "OSSL_DECODER_CTX_set_pem_password_cb PEM failed.\n");
+        goto out;
+    }
+    while (1) {
+        if (OSSL_DECODER_from_bio(dctx, bio) == 1) {
+            fprintf(stderr, "OSSL_DECODER_from_bio PEM failed.\n");
+            goto out;
+        }
+        if (BIO_eof(bio))
+            break;
+        pos2 = BIO_tell(bio);
+        if (pos2 < 0 || pos2 <= pos)
+            break;
+        ERR_clear_error();       /* Maybe print? */
+        pos = pos2;
+    }
+
+    BIO_reset(bio);
+    OSSL_DECODER_CTX_free(dctx);
+    dctx = NULL;
+    dctx = OSSL_DECODER_CTX_new_for_pkey(&pkey, "PEM", NULL, NULL, 0, NULL, NULL);
+    if (!dctx) {
+        fprintf(stderr, "OSSL_DECODER_CTX_new_for_pkey PEM 2 failed.\n");
+        goto out;
+    }
+    if (OSSL_DECODER_CTX_set_pem_password_cb(dctx, PEM_def_callback, ppass) != 1) {
+        fprintf(stderr, "OSSL_DECODER_CTX_set_pem_password_cb PEM 2 failed.\n");
+        goto out;
+    }
+    while (1) {
+        if (OSSL_DECODER_from_bio(dctx, bio) == 1) {
+            fprintf(stderr, "OSSL_DECODER_from_bio PEM 2 failed.\n");
+            goto out;
+        }
+        if (BIO_eof(bio))
+            break;
+        pos2 = BIO_tell(bio);
+        if (pos2 < 0 || pos2 <= pos)
+            break;
+        ERR_clear_error();       /* Maybe print? */
+        pos = pos2;
+    }
+
+    if (!pkey) {
+        fprintf(stderr, "[DEBUG] Failed to get the pkey.\n");
+    }
+out:
+    OSSL_DECODER_CTX_free(dctx);
+    return pkey;
+}
+
+int main(int argc, char *argv[])
+{
+    int status = EXIT_SUCCESS;
+    int fips_enabled = 0;
+    /* Input file */
+    FILE *f = NULL;
+    static char data[1024 * 1024];
+    size_t data_size = 0;
+    EVP_PKEY *pkey = NULL;
+    BIO *bio = NULL;
+    BUF_MEM *buf = NULL;
+
+    /* Print debugging info for FIPS */
+    printf("[DEBUG] Loaded providers:\n");
+    OSSL_PROVIDER_do_all(NULL, &print_provider, NULL);
+    fips_enabled = EVP_default_properties_is_fips_enabled(NULL);
+    printf("[DEBUG] FIPS mode enabled: %d\n", fips_enabled);
+
+    /* Check if the input file from the first argument is valid. */
+    if ((f = fopen(argv[1], "r")) == NULL
+        || (data_size = fread(data, 1, sizeof(data), f)) == 0) {
+        fprintf(stderr, "[DEBUG] Could not read PKey: %s\n", argv[1]);
+        status = EXIT_FAILURE;
+        goto end;
+    }
+    printf("[DEBUG] data_size: %d\n", data_size);
+
+    fclose(f);
+    f = NULL;
+
+    data[data_size] = '\0';
+
+    /* Get Pkey from data. */
+    bio = BIO_new_mem_buf(data, strlen(data));
+    if (!bio) {
+        fprintf(stderr, "[DEBUG] BIO_new_mem_buf() failed\n");
+        status = EXIT_FAILURE;
+        goto end;
+    }
+    pkey = ossl_pkey_read_generic(bio, "");
+    BIO_free(bio);
+    bio = NULL;
+    if (!pkey) {
+        fprintf(stderr, "[DEBUG] Could not parse a PKey\n");
+        status = EXIT_FAILURE;
+        goto end;
+    }
+
+    printf("[DEBUG] Got a pkey! %p\n", (void *)pkey);
+    printf("[DEBUG] It's held by the provider %s\n",
+           OSSL_PROVIDER_get0_name(EVP_PKEY_get0_provider(pkey)));
+
+    /* ossl_pkey_export_spki(pkey, 0); */
+    /* Export Pkey. */
+    bio = BIO_new(BIO_s_mem());
+    if (!bio) {
+        fprintf(stderr, "BIO_new\n");
+        status = EXIT_FAILURE;
+        goto end;
+    }
+    if (!PEM_write_bio_PUBKEY(bio, pkey)) {
+        fprintf(stderr, "PEM_write_bio_PUBKEY\n");
+        status = EXIT_FAILURE;
+        goto end;
+    }
+    BIO_get_mem_ptr(bio, &buf);
+    printf("[DEBUG] ossl_membio2str buf->data:\n%s\n", buf->data);
+end:
+    if (f) {
+        fclose(f);
+    }
+    if (bio) {
+        BIO_free(bio);
+    }
+    if (pkey) {
+        EVP_PKEY_free(pkey);
+    }
+    return status;
+}
